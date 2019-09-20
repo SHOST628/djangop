301c761bf94098f4f5ed627cb5082a0a91b1b597
Sincave <599531369@qq.com>
2019-09-20 09:44:10 +0800

    add model table

--- booktest/models.py
+++ booktest/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# library class
+class BookInfo(models.Model):
+    #  CharField : a string, max_length:max length
+    btitle = models.CharField(max_length=20)
+    #  publish date: type:date
+    bpub_date = models.DateField()
